abfd890cc2e4a142239cc128bad1fa111443a6b0
Quynh Le <quynh.le@employmenthero.com>
2022-08-19 21:50:18 +0700

    Initial Commit

---
+++ Shared/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  Shared
+//
+//  Created by Le Helen on 19/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
